2ab289db157c9a0f03c9eb10bd730587d465e920
Jonathan Macedo Castro <jonathan.castro18@outlook.com>
2025-08-05 11:37:30 -0300

    feat: Criar uma nova rota para usar o HTML e um teste novo para deixar todas as linhas testadas.

--- tests/test_app.py
+++ tests/test_app.py
@@ -22,3 +22,11 @@ def test_root_deve_retornar_ola_mundo():
     # assert
     assert response.json() == {'message': 'Olá mundo'}
     assert response.status_code == HTTPStatus.OK
+
+
+def test_dex_deve_retornar_ola_mundo_html():
+    client = TestClient(app)
+    response = client.get('/dex')
+
+    assert response.status_code == HTTPStatus.OK
+    assert '<h1> Olá Mundo </h1>' in response.text
