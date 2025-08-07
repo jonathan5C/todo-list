35f431f77b586640cf97f7e5cc7303701a8a2915
Jonathan Macedo Castro <jonathan.castro18@outlook.com>
2025-08-07 11:26:51 -0300

    test: Criar um teste para o endpoint POST.

--- tests/test_app.py
+++ tests/test_app.py
@@ -30,3 +30,24 @@ def test_dex_deve_retornar_ola_mundo_html():
 
     assert response.status_code == HTTPStatus.OK
     assert '<h1> Olá Mundo </h1>' in response.text
+
+
+def test_create_user():
+    client = TestClient(app)
+
+    response = client.post(
+        '/users/',
+        json={
+            'id': 0,
+            'username': 'alice',
+            'email': 'alice@exemple.com',
+            'password': 'secret',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'id': 1,
+        'username': 'alice',
+        'email': 'alice@exemple.com',
+    }
