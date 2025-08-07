8af14a71c0d0490f93a50e27320b518c03c2e81f
Jonathan Macedo Castro <jonathan.castro18@outlook.com>
2025-08-07 11:37:29 -0300

    test: Criar uma configuração de teste para não ficar repetindo código sem necessidade.

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,21 +1,13 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fastapi_zero.app import app
-
-
-def test_root_deve_retornar_ola_mundo():
+def test_root_deve_retornar_ola_mundo(client):
     """
     Esse teste é composto por 3 etapas (AAA)
     - A: Arrange (Arranjo)
     - A: Act (Agir)
     - A: Assert (Garantia)
     """
-
-    # arrange
-    client = TestClient(app)
-
     # act
     response = client.get('/')
 
@@ -24,17 +16,13 @@ def test_root_deve_retornar_ola_mundo():
     assert response.status_code == HTTPStatus.OK
 
 
-def test_dex_deve_retornar_ola_mundo_html():
-    client = TestClient(app)
+def test_dex_deve_retornar_ola_mundo_html(client):
     response = client.get('/dex')
-
     assert response.status_code == HTTPStatus.OK
     assert '<h1> Olá Mundo </h1>' in response.text
 
 
-def test_create_user():
-    client = TestClient(app)
-
+def test_create_user(client):
     response = client.post(
         '/users/',
         json={
@@ -44,7 +32,6 @@ def test_create_user():
             'password': 'secret',
         },
     )
-
     assert response.status_code == HTTPStatus.CREATED
     assert response.json() == {
         'id': 1,
